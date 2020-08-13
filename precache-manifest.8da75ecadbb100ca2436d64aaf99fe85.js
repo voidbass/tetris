e9c3ba78eec23d5c00203ebee3de0d24e6fa1417
Khôi Huỳnh <khoi.dn95@gmail.com>
2020-08-13 21:12:28 +0700

    Updates

---
+++ precache-manifest.8da75ecadbb100ca2436d64aaf99fe85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98420714cc00cabb97ee146f73ad7d9",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "41e21d114d42e35c2749",
+    "url": "/tetris/static/css/main.c8b7d97c.chunk.css"
+  },
+  {
+    "revision": "2c42c847072594b0fbfd",
+    "url": "/tetris/static/js/2.a4347658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris/static/js/2.a4347658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e21d114d42e35c2749",
+    "url": "/tetris/static/js/main.5d72e2b8.chunk.js"
+  },
+  {
+    "revision": "c179afea1aa9726cb16b",
+    "url": "/tetris/static/js/runtime-main.0d8747ab.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris/static/media/bg.2810fe6b.png"
+  }
+]);
